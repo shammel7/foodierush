a0d39dbd837984baffce5f240c99e4964872a997
shammel7 <shammelquilapio@gmail.com>
2025-05-31 18:45:15 +0800

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,8 @@
+<?php session_start(); ?>
+<link rel="stylesheet" href="style.css">
+<div class="container">
+  <h2>Thank You For Trusting FoodieRush!</h2>
+  <p>ENJOY YOUR FOOD</p>
+  <a href="logout.php"><button>Log Out</button></a>
+</div>
+<?php session_destroy(); ?>
